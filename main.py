e6750f43e19d1872111f6ea4c6cd0bdd642b3794
xiaxin1998 <38903481+xiaxin1998@users.noreply.github.com>
2021-03-10 12:21:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ parser.add_argument('--embSize', type=int, default=100, help='embedding size')
 parser.add_argument('--l2', type=float, default=1e-5, help='l2 penalty')
 parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
 parser.add_argument('--layer', type=float, default=3, help='the number of layer used')
-parser.add_argument('--beta', type=float, default=0.05, help='ssl task maginitude')
+parser.add_argument('--beta', type=float, default=0.01, help='ssl task maginitude')
 parser.add_argument('--filter', type=bool, default=False, help='filter incidence matrix')
 
 opt = parser.parse_args()
